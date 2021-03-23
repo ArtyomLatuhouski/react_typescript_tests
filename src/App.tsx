29c8b9a59fbef4edccf65cc89875e9905a87e5a3
Artsem Latuohuski <ipohhop@gmail.com>
2021-03-23 22:16:40 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="body__container">
+       hello world
+    </div>
+  );
+}
+
+export default App;
