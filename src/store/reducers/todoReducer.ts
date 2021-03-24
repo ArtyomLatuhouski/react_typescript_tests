244923f9182792d040d01cffbcb41695d006ae37
Artsem Latuohuski <artsem.latuohuski@innowise-group.com>
2021-03-24 11:11:02 +0300

    creat todo reducer carcass and todo reducer utilites (ts interface items )

---
+++ src/store/reducers/todoReducer.ts
@@ -0,0 +1,13 @@
+// outer
+
+
+//local
+import {TodoActions, TodoState} from "../../types/todoReducerUtilites";
+
+
+export const todoReducer = (state: TodoState, action: TodoActions): TodoState => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
---
+++ src/types/todoReducerUtilites.ts
@@ -0,0 +1,42 @@
+export interface TodoState {
+    todos: [],
+    loading: boolean,
+    error: null | string,
+    page: number,
+    limit: number
+}
+
+
+export enum TodoActionsTypes {
+    FETCH_TODO = "FETCH_TODO",
+    FETCH_TODO_SUCCESS = "FETCH_TODO_SUCCESS",
+    FETCH_TODO_ERROR = "FETCH_TODO_ERROR",
+    SET_TODO_PAGE = "SET_TODO_PAGE"
+}
+
+interface FetchTodoAction {
+    type: TodoActionsTypes.FETCH_TODO
+}
+
+interface FetchTodoSuccessAction {
+    type: TodoActionsTypes.FETCH_TODO_SUCCESS,
+    payload: any[]
+}
+
+interface FetchTodoErrorAction {
+    type: TodoActionsTypes.FETCH_TODO_ERROR,
+    payload: string
+
+}
+
+interface SetTodoPageAction {
+    type: TodoActionsTypes.SET_TODO_PAGE,
+    payload: number
+}
+
+
+export type TodoActions =
+      FetchTodoAction
+    | FetchTodoSuccessAction
+    | FetchTodoErrorAction
+    | SetTodoPageAction
